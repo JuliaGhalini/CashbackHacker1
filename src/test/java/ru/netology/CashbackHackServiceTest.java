004b2d9d2a5b15ac0e986f4c033b7d6f0e42e36b
Ghalini Julia <ghalinijulia@gmail.com>
2021-01-21 16:05:09 +0300

    fix method "shouldCalculateHackService"

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateHackService(){
+    public void shouldCalculateHackService(){
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
 
